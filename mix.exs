381f29d038e484749dba4382307f25e0ad61ede2
data-maestro <re_tina@yahoo.co.jp>
2018-01-03 19:37:55 +0900

    API series move to new library "wrapper", Add performance series

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Smallex.Mixfile do
   def project do
     [
       app: :smallex,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.5",
 		description: "Elixir small utilities", 
 		package: 
@@ -29,33 +29,28 @@ defmodule Smallex.Mixfile do
 	defp deps do
 		[
 			{ :ex_doc,         "~> 0.18.1", only: :dev, runtime: false }, 
-			{ :earmark,        "~> 1.2.4" }, 
+			{ :earmark,        "~> 1.2.4",  only: :dev }, 
 			{ :power_assert,   "~> 0.1.1",  only: :test }, 
 			{ :mix_test_watch, "~> 0.5.0",  only: :dev, runtime: false }, 
-			{ :dialyxir,       "~> 0.5.1" }, 
+			{ :dialyxir,       "~> 0.5.1",  only: :dev }, 
 
 			{ :timex,          "~> 3.1.24" }, 
 			{ :math,           "~> 0.3.0" }, 
 			{ :complex_num,    "~> 1.0.0" }, 
 
 			{ :nimble_csv,     "~> 0.4.0" }, 
-
 			{ :ecto,           "~> 2.2.6" }, 
+			{ :stash,               "~> 1.0.0" }, 
+			{ :memoize,             "~> 1.2.4" }, 
+			{ :logger_file_backend, "~> 0.0.10" }, 
 
-			{ :httpoison,      "~> 0.13.0" }, 
-			{ :poison,         "~> 3.1.0" }, 
-			{ :floki,          "~> 0.19.2" },
 
 			{ :combine,        "~> 0.10.0" }, 
 
-			{ :scrivener_html, "~> 1.7.1" }, 
-			{ :scrivener_list, "~> 1.0.1" }, 
-
 			{ :quantum,        "~> 2.2.0" }, 
 
-			{ :slack,          "~> 0.12.0" }, 
-
 			{ :flow,           "~> 0.12.0" }, 
+			{ :benchfella,          "~> 0.3.5" }, 
 		]
 	end
 end
--- test/smallex_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SmallexTest do
-  use ExUnit.Case
-  doctest Smallex
-
-  test "greets the world" do
-    assert Smallex.hello() == :world
-  end
-end
