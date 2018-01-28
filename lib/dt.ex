6c8ac2069904c96c16def48c7199fefadb560534
data-maestro <re_tina@yahoo.co.jp>
2018-01-28 20:21:38 +0900

    Add Dt.to_datetime()

--- lib/dt.ex
+++ lib/dt.ex
@@ -1,4 +1,8 @@
 defmodule Dt do
+	@moduledoc """
+	DateTime utiity.
+	"""
+
 	def now_timestamp( date_sep \\ "", separate \\ "", time_sep \\ "", second_sep \\ "" ) do
 		Timex.now |> to_timestamp_string( date_sep, separate, time_sep, second_sep )
 	end
@@ -16,4 +20,48 @@ defmodule Dt do
 
 	def now_ymd(), do: Timex.now |> to_ymd_string
 	def to_ymd_string( dt ), do: dt |> format( "{YYYY}/{0M}/{0D}" )
+
+	@doc """
+	Get datetime from tuple
+
+	## Examples
+		iex> Dt.to_datetime( { { 2018, 1, 22 }, { 23, 44, 9 } }" )
+		~N[2018-01-22 23:44:09]
+
+		iex> Dt.to_datetime( "{ 2018, 1, 22 }" )
+		~N[2018-01-22 00:00:00]
+	"""
+	def to_datetime( tuple ) when is_tuple( tuple ) do
+		tuple
+		|> Timex.to_datetime
+		|> DateTime.to_string
+		|> String.slice( 0..-2 )
+	end
+
+	@doc """
+	Get datetime from string (NOTE: Define this after to_datetime( tuple ))
+
+	## Examples
+		iex> Dt.to_datetime( "2018/01/22 23:44:09" )
+		~N[2018-01-22 23:44:09]
+
+		iex> Dt.to_datetime( "2018/01/22" )
+		~N[2018-01-22 00:00:00]
+
+		iex> Dt.to_datetime( "2018-01-22T23:44:09Z" )
+		~N[2018-01-22 23:44:09]
+	"""
+	def to_datetime( str ) when is_binary( str ) do
+		[
+			"%Y/%m/%d", 
+			"%Y/%m/%d %H:%M", 
+			"%Y/%m/%d %H:%M:%S", 
+			"%Y/%m/%d %H:%M:%S.%L", 
+			"%Y-%m-%dT%H:%M:%SZ", 
+		]
+		|> Enum.map( &( str |> Timex.parse( &1, :strftime ) ) )
+		|> Enum.filter( &elem( &1, 0 ) == :ok )
+		|> List.first
+		|> Tpl.ok
+	end
 end
