d216cca0a0dd92a7c7cf78c0c3f5f3f1a82b8ad4
data-maestro <re_tina@yahoo.co.jp>
2018-01-28 21:06:12 +0900

    Merge to_datetime() string/tuple

--- lib/dt.ex
+++ lib/dt.ex
@@ -22,24 +22,7 @@ defmodule Dt do
 	def to_ymd_string( dt ), do: dt |> format( "{YYYY}/{0M}/{0D}" )
 
 	@doc """
-	Get datetime from tuple
-
-	## Examples
-		iex> Dt.to_datetime( { { 2018, 1, 22 }, { 23, 44, 9 } }" )
-		~N[2018-01-22 23:44:09]
-
-		iex> Dt.to_datetime( "{ 2018, 1, 22 }" )
-		~N[2018-01-22 00:00:00]
-	"""
-	def to_datetime( tuple ) when is_tuple( tuple ) do
-		tuple
-		|> Timex.to_datetime
-		|> DateTime.to_string
-		|> String.slice( 0..-2 )
-	end
-
-	@doc """
-	Get datetime from string (NOTE: Define this after to_datetime( tuple ))
+	Get datetime from string/tuple
 
 	## Examples
 		iex> Dt.to_datetime( "2018/01/22 23:44:09" )
@@ -50,6 +33,12 @@ defmodule Dt do
 
 		iex> Dt.to_datetime( "2018-01-22T23:44:09Z" )
 		~N[2018-01-22 23:44:09]
+
+		iex> Dt.to_datetime( { { 2018, 1, 22 }, { 23, 44, 9 } } )
+		~N[2018-01-22 23:44:09]
+
+		iex> Dt.to_datetime( { 2018, 1, 22 } )
+		~N[2018-01-22 00:00:00]
 	"""
 	def to_datetime( str ) when is_binary( str ) do
 		[
@@ -58,10 +47,18 @@ defmodule Dt do
 			"%Y/%m/%d %H:%M:%S", 
 			"%Y/%m/%d %H:%M:%S.%L", 
 			"%Y-%m-%dT%H:%M:%SZ", 
+			"%Y-%m-%d %H:%M:%SZ", 
 		]
 		|> Enum.map( &( str |> Timex.parse( &1, :strftime ) ) )
 		|> Enum.filter( &elem( &1, 0 ) == :ok )
 		|> List.first
 		|> Tpl.ok
 	end
+	def to_datetime( tuple ) when is_tuple( tuple ) do
+		tuple
+		|> Timex.to_datetime
+		|> DateTime.to_string
+		|> to_datetime
+	end
+
 end
