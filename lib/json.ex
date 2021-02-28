2fa2242fe7ef4fe51683f9ddd5d2f0ec2ceda265
kenko <universeengineer@gmail.com>
2021-02-28 03:17:02 +0000

    support get and get_raw_response function that do not separate domain and not domain

--- lib/json.ex
+++ lib/json.ex
@@ -10,17 +10,35 @@ defmodule Json do
     iex> Json.get( "https://api.github.com", "/rate_limit" )[ "rate" ][ "limit" ]
     60
 
+    iex> Json.get("https://api.github.com/rate_limit")["rate"]["limit"]
+    60
+
     iex> Json.get_raw_response( "https://api.github.com", "/rate_limit" ).status_code
     200
   """
-  def get_raw_response(domain, path, header \\ ["Content-Type": "application/json"]) do
+  def get_raw_response(url), do: get_raw_response(url, ["Content-Type": "application/json"])
+
+  def get_raw_response(url, header) when is_list(header), do: HTTPoison.get!(url, header)
+
+  def get_raw_response(domain, path), do: get_raw_response(domain, path, ["Content-Type": "application/json"])
+
+  def get_raw_response(domain, path, header) do
     (domain <> path)
-    |> HTTPoison.get!(header)
+    |> get_raw_response(header)
   end
 
-  def get(domain, path, header \\ ["Content-Type": "application/json"]) do
-    get_raw_response(domain, path, header)
+  def get(url), do: get(url, ["Content-Type": "application/json"])
+
+  def get(url, header) when is_list(header) do
+    get_raw_response(url, header)
     |> parse
+  end
+
+  def get(domain, path), do: get(domain, path, ["Content-Type": "application/json"])
+
+  def get(domain, path, header) do
+    (domain <> path)
+    |> get(header)
   end
 
   @doc ~S"""
@@ -51,7 +69,8 @@ defmodule Json do
     iex> Json.post_raw_response( "https://httpbin.org", "/post?param1=value1", [ data1: "value1" ], "Content-Type": "application/json" ).status_code
     200
   """
-  def post_raw_response(domain, path, body), do: post_raw_response(domain, path, body, ["Content-Type": "application/json"])
+  def post_raw_response(domain, path, body),
+    do: post_raw_response(domain, path, body, "Content-Type": "application/json")
 
   def post_raw_response(domain, path, body, header) when is_list(body) do
     post_raw_response(domain, path, body |> Enum.into(%{}), header)
@@ -67,7 +86,7 @@ defmodule Json do
     |> HTTPoison.post!(body, header)
   end
 
-  def post(domain, path, body), do: post(domain, path, body, ["Content-Type": "application/json"])
+  def post(domain, path, body), do: post(domain, path, body, "Content-Type": "application/json")
 
   def post(domain, path, body, header) when is_list(body) do
     post(domain, path, body |> Enum.into(%{}), header)
@@ -108,7 +127,8 @@ defmodule Json do
     iex> Json.put_raw_response( "https://httpbin.org", "/put?param1=value1",  [ data1: "value1" ], "Content-Type": "application/json" ).status_code
     200
   """
-  def put_raw_response(domain, path, body), do: put_raw_response(domain, path, body, ["Content-Type": "application/json"])
+  def put_raw_response(domain, path, body),
+    do: put_raw_response(domain, path, body, "Content-Type": "application/json")
 
   def put_raw_response(domain, path, body, header) when is_list(body) do
     put_raw_response(domain, path, body |> Enum.into(%{}), header)
@@ -124,7 +144,7 @@ defmodule Json do
     |> HTTPoison.put!(body, header)
   end
 
-  def put(domain, path, body), do: put(domain, path, body, ["Content-Type": "application/json"])
+  def put(domain, path, body), do: put(domain, path, body, "Content-Type": "application/json")
 
   def put(domain, path, body, header) when is_list(body) do
     put(domain, path, body |> Enum.into(%{}), header)
@@ -165,7 +185,8 @@ defmodule Json do
     iex> Json.patch_raw_response( "https://httpbin.org", "/patch?param1=value1",  [ data1: "value1" ], "Content-Type": "application/json" ).status_code
     200
   """
-  def patch_raw_response(domain, path, body), do: patch_raw_response(domain, path, body, ["Content-Type": "application/json"])
+  def patch_raw_response(domain, path, body),
+    do: patch_raw_response(domain, path, body, "Content-Type": "application/json")
 
   def patch_raw_response(domain, path, body, header) when is_list(body) do
     patch_raw_response(domain, path, body |> Enum.into(%{}), header)
@@ -181,7 +202,7 @@ defmodule Json do
     |> HTTPoison.patch!(body, header)
   end
 
-  def patch(domain, path, body), do: patch(domain, path, body, ["Content-Type": "application/json"])
+  def patch(domain, path, body), do: patch(domain, path, body, "Content-Type": "application/json")
 
   def patch(domain, path, body, header) when is_list(body) do
     patch(domain, path, body |> Enum.into(%{}), header)
